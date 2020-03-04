4061695d37ff1bd216ab6b5eecd5580b98a774c5
Jordan Furr <you@example.com>
2020-03-04 11:43:22 -0700

    Core Data Stack

---
+++ PlaylistCoredata/PlaylistCoredata/Model/Playlist+Convenience.swift
@@ -0,0 +1,16 @@
+//
+//  Playlist+Convenience.swift
+//  PlaylistCoredata
+//
+//  Created by Jordan Furr on 3/4/20.
+//  Copyright © 2020 Trevor Walker. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Playlist {
+    convenience init(name: String, songs: [Song] = [], moc: NSManagedObjectContext){
+        
+    }
+}
